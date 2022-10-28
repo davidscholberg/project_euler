ecc0674f9d33f1aa1dc31eb650c9f7aed9aebca2
David Scholberg <recombinant.vector@gmail.com>
2022-10-28 15:40:59 -0400

    fixed variable name

--- project_euler/solutions/s0014.py
+++ project_euler/solutions/s0014.py
@@ -3,4 +3,4 @@ from project_euler.util.sequences.collatz import Collatz
 def get_answer() -> int:
     collatz = Collatz()
     input_length_pairs = map(lambda n: (n, collatz.sequence_length(n)), range(1, 1000000))
-    return max(input_length_pairs, key=lambda n: n[1])[0]
+    return max(input_length_pairs, key=lambda t: t[1])[0]
