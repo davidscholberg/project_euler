d62dbd0cc3f3e97b1559e91bfee72fb2e3d4a04a
David Scholberg <recombinant.vector@gmail.com>
2022-11-02 20:53:33 -0400

    added solution timing to output

--- project_euler/solve.py
+++ project_euler/solve.py
@@ -1,6 +1,7 @@
 import argparse
 import importlib
 import sys
+import time
 
 from project_euler.util.answers import answer_list
 
@@ -10,9 +11,12 @@ argparser.add_argument("problem_number", help="The project euler problem number 
 args = argparser.parse_args()
 try:
     problem_number_module = importlib.import_module(f"project_euler.solutions.s{args.problem_number:04}")
-    your_answer = problem_number_module.get_answer()
-    correct_answer = answer_list[args.problem_number]
     print(f"solution {args.problem_number}")
+    start_time = time.process_time_ns()
+    your_answer = problem_number_module.get_answer()
+    total_time = time.process_time_ns() - start_time
+    print(f"time taken: {total_time / 1000000000:.3f}s")
+    correct_answer = answer_list[args.problem_number]
     print(f"your answer:    {your_answer}")
     print(f"correct answer: {correct_answer}")
     if your_answer == correct_answer:
