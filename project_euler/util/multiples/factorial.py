ed25af41fc971b897af0be2833a6b2b104eeefa6
David Scholberg <recombinant.vector@gmail.com>
2022-11-08 18:42:51 -0500

    refactored solution 34

--- project_euler/util/multiples/factorial.py
+++ project_euler/util/multiples/factorial.py
@@ -1,4 +1,13 @@
 import math
 
-def factorial(n: int) -> int:
+def factorial(n: int, cache: None | dict = None) -> int:
+    if cache is not None:
+        if n in cache:
+            return cache[n]
+        if n == 0:
+            cache[n] = 1
+            return 1
+        result = n * factorial(n - 1, cache=cache)
+        cache[n] = result
+        return result
     return math.prod(range(2, n + 1))
