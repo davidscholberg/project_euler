9d806360546b1cb4103a036dd8646b098db60bee
David Scholberg <recombinant.vector@gmail.com>
2022-11-24 21:19:31 -0500

    reduced domain of solution 5

--- project_euler/solutions/s0005.py
+++ project_euler/solutions/s0005.py
@@ -1,4 +1,4 @@
 from project_euler.util.multiples.multiples import smallest_number_divisible_by
 
 def get_answer() -> int:
-    return smallest_number_divisible_by(range(2, 21))
+    return smallest_number_divisible_by((11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
